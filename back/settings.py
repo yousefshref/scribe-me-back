dee8dd0d0aa36579371e69cde74d20de5f0b6e46
yousefshref <yb2005at@gmail.com>
2024-12-15 20:54:49 +0200

    a

--- back/settings.py
+++ back/settings.py
@@ -80,12 +80,25 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scribeme',
+        'USER': 'scribeme',
+        'PASSWORD': '01095753871Ysmb#@',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
